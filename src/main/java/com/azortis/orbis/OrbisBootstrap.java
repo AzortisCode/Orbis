35c19eb6514722ceaae34868eeaf7ae15bceb670
Jake Nijssen <jtmnijssen@outlook.com>
2021-01-20 01:38:42 +0100

    Implemented license

--- src/main/java/com/azortis/orbis/OrbisBootstrap.java
+++ src/main/java/com/azortis/orbis/OrbisBootstrap.java
@@ -1,6 +1,30 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Azortis
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.azortis.orbis;
 
-import com.azortis.orbis.generator.OrbisChunkGenerator;
+import com.azortis.orbis.generator.OrbisChunkProvider;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.entity.GameMode;
 import net.minestom.server.entity.Player;
@@ -10,7 +34,6 @@ import net.minestom.server.event.player.PlayerLoginEvent;
 import net.minestom.server.event.player.PlayerSpawnEvent;
 import net.minestom.server.extras.optifine.OptifineSupport;
 import net.minestom.server.instance.InstanceContainer;
-import net.minestom.server.storage.StorageLocation;
 import net.minestom.server.storage.StorageManager;
 import net.minestom.server.storage.systems.FileStorageSystem;
 import net.minestom.server.utils.Position;
@@ -22,22 +45,19 @@ public class OrbisBootstrap {
 
     public static void main(String[] args){
         MinecraftServer minecraftServer = MinecraftServer.init();
+        MinecraftServer.setBrandName("Orbis Development");
 
         StorageManager storageManager = MinecraftServer.getStorageManager();
         storageManager.defineDefaultStorageSystem(FileStorageSystem::new);
 
-        StorageLocation storageLocation = storageManager.getLocation("chunk_data");
-
-        InstanceContainer instanceContainer = MinecraftServer.getInstanceManager().createInstanceContainer(DimensionType.OVERWORLD, storageLocation);
-        instanceContainer.setChunkGenerator(new OrbisChunkGenerator());
+        InstanceContainer instanceContainer = MinecraftServer.getInstanceManager().createInstanceContainer(DimensionType.OVERWORLD);
+        instanceContainer.setChunkGenerator(new OrbisChunkProvider());
         instanceContainer.enableAutoChunkLoad(true);
         for (int x = -16; x <= 16; x++){
             for (int z = -16; z <= 16; z++){
                 instanceContainer.loadChunk(x, z);
             }
         }
-        instanceContainer.saveChunksToStorage();
-
         OptifineSupport.enable();
 
         GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
--- src/main/java/com/azortis/orbis/generator/terrain/BaseTerrain.java
+++
@@ -1,7 +0,0 @@
-package com.azortis.orbis.generator.terrain;
-
-public abstract class BaseTerrain {
-
-    public abstract float getTerrainHeight(int x, int z);
-
-}
