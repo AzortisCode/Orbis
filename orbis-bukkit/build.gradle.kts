1f44da2bb192c59f4070f85d61a77d4fabd9ce58
Jake Nijssen <jtmnijssen@outlook.com>
2021-03-01 23:41:37 +0100

    Add latest

--- orbis-bukkit/build.gradle.kts
+++ orbis-bukkit/build.gradle.kts
@@ -7,11 +7,20 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    mavenLocal()
+    jcenter()
     maven {
         url = uri("https://papermc.io/repo/repository/maven-public/")
+    }
+    maven {
+        url = uri("https://libraries.minecraft.net")
+    }
+    maven {
+        url = uri("https://jitpack.io")
     }
 }
 
 dependencies {
+    implementation(project(":orbis-core"))
     compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
 }
--- orbis-minestom/build.gradle.kts
+++ orbis-minestom/build.gradle.kts
@@ -7,8 +7,22 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    mavenLocal()
+    jcenter()
+    maven {
+        url = uri("https://repo.spongepowered.org/maven")
+    }
+
+    maven {
+        url = uri("https://libraries.minecraft.net")
+    }
+
+    maven {
+        url = uri("https://jitpack.io")
+    }
 }
 
 dependencies {
-
+    implementation(project(":orbis-core"))
+    implementation("com.github.Minestom:Minestom:3026e46220")
 }
