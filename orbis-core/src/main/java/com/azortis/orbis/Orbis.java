dba79f0815b3db877ee8588758e58ee9a6fb6da6
Jake Nijssen <jtmnijssen@outlook.com>
2021-03-02 20:17:17 +0100

    MORE!

--- orbis-core/src/main/java/com/azortis/orbis/Orbis.java
+++ orbis-core/src/main/java/com/azortis/orbis/Orbis.java
@@ -24,139 +24,41 @@
 
 package com.azortis.orbis;
 
-import com.azortis.orbis.generator.terrain.Terrain;
-import com.azortis.orbis.instance.InstanceManager;
-import com.azortis.orbis.registry.*;
-import com.azortis.orbis.registry.adapter.NamespaceIdAdapter;
-import com.azortis.orbis.registry.adapter.TerrainAdapter;
-import com.azortis.orbis.util.NamespaceID;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.io.*;
-import java.util.Objects;
+import java.io.File;
 
-@SuppressWarnings("unused")
 public final class Orbis {
 
-    // Generic fields
-    private static boolean initialised = false;
-    private static final Logger logger = LoggerFactory.getLogger(Orbis.class);
-    private static Gson gson;
-    private static File directory;
-    private static File settingsFile;
-    private static OrbisSettings settings;
+    private static Adapter adapter = null;
+    private static Logger logger;
 
-    // Managers
-    private static InstanceManager instanceManager;
+    private static File packDirectory;
 
-    // Registries
-    private static DimensionRegistry dimensionRegistry;
-    private static BiomeRegistry biomeRegistry;
-    private static TerrainRegistry terrainRegistry;
-
-    private Orbis() {
+    private Orbis(){
     }
 
-    public static void initialise(File directory) {
-        if (!initialised) {
-            initialised = true;
-            Orbis.directory = directory;
-            if(!directory.exists()){
-                if(!directory.mkdirs())logger.error("Couldn't create orbis directory!");
+    public static void initialize(Adapter adaptation){
+        if(adapter == null){
+            adapter = adaptation;
+            logger = adaptation.getLogger();
+            logger.info("Initializing {} adaptation of Orbis", adaptation.getAdaptation());
+
+            // Load all packs
+            logger.info("Loading all packs...");
+            packDirectory = new File(adapter.getDirectory(), "/packs/");
+            if(!packDirectory.exists()){
+
             }
-
-            // Load registries
-            dimensionRegistry = new DimensionRegistry();
-            biomeRegistry = new BiomeRegistry();
-            terrainRegistry = new TerrainRegistry();
-
-            // Gson
-            GsonBuilder gsonBuilder = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping();
-            gsonBuilder.registerTypeAdapter(NamespaceID.class, new NamespaceIdAdapter());
-            gsonBuilder.registerTypeAdapter(Terrain.class, new TerrainAdapter(terrainRegistry));
-            gson = gsonBuilder.create();
-
-            // Load settings file
-            settingsFile = new File(directory, "settings.json");
-            try{
-                if(!settingsFile.exists())FileUtils.copyURLToFile(Objects.requireNonNull(Orbis.class.getClassLoader().getResource("settings.json")), settingsFile);
-                settings = gson.fromJson(new FileReader(settingsFile), OrbisSettings.class);
-            } catch (IOException ex){
-                logger.error("Something went wrong with loading the settings file.");
-            }
-
-            // Load managers
-            instanceManager = new InstanceManager(settings);
-
         }
     }
 
-    public static boolean isInitialised() {
-        return initialised;
+    public static Adapter getAdapter() {
+        return adapter;
     }
 
     public static Logger getLogger() {
         return logger;
-    }
-
-    public static Gson getGson() {
-        return gson;
-    }
-
-    public static File getDirectory() {
-        return directory;
-    }
-
-    public static File getSettingsFile() {
-        return settingsFile;
-    }
-
-    public static OrbisSettings getSettings() {
-        return settings;
-    }
-
-    // Managers
-
-    public static InstanceManager getInstanceManager() {
-        return instanceManager;
-    }
-
-
-    // Registries
-
-    public static DimensionRegistry getDimensionRegistry() {
-        return dimensionRegistry;
-    }
-
-    public static RegionRegistry getRegionRegistry() {
-        return null;
-    }
-
-    public static BiomeRegistry getBiomeRegistry() {
-        return biomeRegistry;
-    }
-
-    public static TerrainRegistry getTerrainRegistry() {
-        return terrainRegistry;
-    }
-
-    private static void copy(InputStream in, File file) {
-        try {
-            OutputStream out = new FileOutputStream(file);
-            byte[] buf = new byte[1024];
-            int len;
-            while((len=in.read(buf))>0){
-                out.write(buf,0,len);
-            }
-            out.close();
-            in.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
 }
